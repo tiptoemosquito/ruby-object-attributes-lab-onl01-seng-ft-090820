2d3f02e13cf72c0963e1d123c4448cdd73f72399
Christina Howard <clhoward33@icloud.com>
2020-09-16 03:54:23 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def name=(my_dogs_name)
+    @name_of_dog = my_dogs_name
+  end
+
+  def name
+    @name_of_dog
+  end
+
+  def breed(my_dogs_breed)
+    @breed_of_dog = my_dogs_breed
+  end
+
+  def breed
+    @breed_of_dog
+  end
+end
